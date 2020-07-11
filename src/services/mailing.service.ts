b96bbc179cb03b8ef957ab0af5cebb9b8d92b116
Florent HUGOUVIEUX <florent.hugouvieux@protonmail.com>
2020-07-11 22:50:28 +0200

    Improve email style

--- src/services/mailing.service.ts
+++ src/services/mailing.service.ts
@@ -24,7 +24,83 @@ export class NodeMailerMailingService implements MailingService {
             to: user.email,
             subject: 'Inscription sur le site web du BDE',
             text: `Inscrivez-vous sur le site du BDE à l'url suivante : ${frontURL}/account/confirm?uuid=${user.uuid}`,
-            html: `Inscrivez vous site le site du BDE <a href="${frontURL}/account/confirm?uuid=${user.uuid}">ici</a>`
+            html: `
+                    <style>
+                        .container {
+                            padding: 0 10px;
+                            width: 500px;
+                            border-left: 3px solid #003865;
+                            border-right: 3px solid #003865;
+                            font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+                        }
+                    
+                        .header {
+                            display: flex;
+                            flex-direction: row;
+                            vertical-align: middle;
+                            background-color: #003865;
+                            color: white;
+                        }
+                    
+                        .header img {
+                            height: 72px;
+                            background-color: white;
+                            border-radius: 100%;
+                            margin: 10px;
+                            margin-right: 40px;
+                        }
+                    
+                        .content {
+                            color: #222222;
+                        }
+                    
+                        .register {
+                            display: block;
+                            width: 70%;
+                            text-align: center;
+                            font-size: 1.5em;
+                            margin: 0 15%;
+                            margin-top: 2em;
+                            padding: 5px;
+                            box-sizing: border-box;
+                            background-color: #003865;
+                            text-decoration: none;
+                            color: white;
+                        }
+                    
+                        .footer {
+                            margin-top: 40px;
+                            width: 100%;
+                            font-size: x-small;
+                            color: grey;
+                        }
+                    
+                        .footer a {
+                            color: grey;
+                        }
+                    </style>
+                    
+                    <div class="container">
+                        <div class="header">
+                            <img src="${frontURL}/assets/icons/icon-512x512.png">
+                            <h1>BDE Polytech</h1>
+                        </div>
+                    
+                        <p class="content">
+                            Bonjour,<br><br>
+                            Vous êtes invité à vous inscrire sur le site du BDE de Polytech afin de pouvoir accéder à la réservation des événements organisés par celui-ci.
+                        </p>
+                    
+                        <a class="register" href="${frontURL}/account/confirm?uuid=${user.uuid}">
+                            S'inscrire
+                        </a>
+                    
+                        <div class="footer">
+                            Site du BDE : <a href="#">${frontURL}</a><br>
+                            Contact <a href="mailto:florent.hugouvieux@protonmail.com">"Florent HUGOUVIEUX" &lt;florent.hugouvieux@protonmail.com&gt;</a>
+                        </div>
+                    </div>        
+            `
         });
 
         const previewURL = nodemailer.getTestMessageUrl(info);
