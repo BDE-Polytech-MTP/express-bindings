cdfa6d44c3cbb2fd6daa738977b6f3030b8afded
Florent HUGOUVIEUX <florent.hugouvieux@protonmail.com>
2020-09-10 17:47:18 +0200

    Adapt to bookings service specifications changes

--- src/services/bookings.service.ts
+++ src/services/bookings.service.ts
@@ -1,23 +1,29 @@
-import { BookingsServiceError, BookingsErrorType, BookingsService, Booking, Event } from "@bde-polytech-mtp/base-backend";
+import { BookingsServiceError, BookingsErrorType, BookingsService, Booking, Event, PartialUser } from "@bde-polytech-mtp/base-backend";
 import { Pool } from 'pg';
 import { DateTime } from 'luxon';
 
 interface BookingRow {
     user_uuid: string;
     event_uuid: string;
+
     event_name: string;
     booking_start: Date | null;
     booking_end: Date | null;
     event_date: Date | null;
     bde_uuid: string;
     is_draft: boolean;
+
+    firstname: string | null;
+    lastname: string | null;
+    specialty_name: string | null;
+    specialty_year: number | null;
 }
 
 export class PostgresBookingsService implements BookingsService {
 
     constructor(private db: Pool) {}
 
-    private mapBookingRowToBookingAndEvent(row: BookingRow): Booking & Event {
+    private mapBookingRowToBookingAndEventAndUser(row: BookingRow): Booking & Event & PartialUser {
         return {
             eventUUID: row.event_uuid,
             userUUID: row.user_uuid,
@@ -27,6 +33,10 @@ export class PostgresBookingsService implements BookingsService {
             bookingEnd: row.booking_end ? DateTime.fromJSDate(row.booking_end) : undefined,
             bookingStart: row.booking_start ? DateTime.fromJSDate(row.booking_start) : undefined,
             eventDate: row.event_date ? DateTime.fromJSDate(row.event_date) : undefined,
+            firstname: row.firstname || '',
+            lastname: row.lastname || '',
+            specialtyName: row.specialty_name || '',
+            specialtyYear: row.specialty_year || 0,
         };
     }
 
@@ -54,15 +64,15 @@ export class PostgresBookingsService implements BookingsService {
         }
     }
 
-    async findOne(userUUID: string, eventUUID: string): Promise<Booking & Event> {
+    async findOne(userUUID: string, eventUUID: string): Promise<Booking & Event & PartialUser> {
         try {
             const { rows } : { rows: BookingRow[] } = await this.db.query(
-                'SELECT * FROM bookings NATURAL JOIN events WHERE event_uuid=$1 AND user_uuid=$2',
+                'SELECT * FROM bookings NATURAL JOIN events NATURAL JOIN users WHERE event_uuid=$1 AND user_uuid=$2',
                 [eventUUID, userUUID]
             );
             if (rows.length > 0) {
                 const row = rows[0];
-                return this.mapBookingRowToBookingAndEvent(row);
+                return this.mapBookingRowToBookingAndEventAndUser(row);
             }
             return Promise.reject(new BookingsServiceError('Not found.', BookingsErrorType.BOOKING_NOT_EXISTS));
         } catch (e) {
@@ -70,19 +80,24 @@ export class PostgresBookingsService implements BookingsService {
         }
     }
 
-    async findBookingsForEvent(eventUUID: string): Promise<(Booking & Event)[]> {
+    async findBookingsForEvent(eventUUID: string): Promise<(Booking & Event & PartialUser)[]> {
         try {
-            const { rows } : { rows: BookingRow[] } = await this.db.query('SELECT * FROM bookings NATURAL JOIN events WHERE event_uuid=$1', [eventUUID]);
-            return rows.map(this.mapBookingRowToBookingAndEvent);
+            const { rows } : { rows: BookingRow[] } = await this.db.query(
+                'SELECT * FROM bookings NATURAL JOIN events NATURAL JOIN users WHERE event_uuid=$1',
+                [eventUUID]
+            );
+            return rows.map(this.mapBookingRowToBookingAndEventAndUser);
         } catch (e) {
             throw new BookingsServiceError(`Unable to fetch bookings for event ${eventUUID}.\n${e}`, BookingsErrorType.INTERNAL);
         }
     }
 
-    async findBookingsForUser(userUUID: string): Promise<(Booking & Event)[]> {
+    async findBookingsForUser(userUUID: string): Promise<(Booking & Event & PartialUser)[]> {
         try {
-            const { rows } : { rows: BookingRow[] } = await this.db.query('SELECT * FROM bookings NATURAL JOIN events WHERE user_uuid=$1', [userUUID]);
-            return rows.map(this.mapBookingRowToBookingAndEvent);
+            const { rows } : { rows: BookingRow[] } = await this.db.query(
+                'SELECT * FROM bookings NATURAL JOIN events NATURAL JOIN users WHERE user_uuid=$1', [userUUID]
+            );
+            return rows.map(this.mapBookingRowToBookingAndEventAndUser);
         } catch (e) {
             throw new BookingsServiceError(`Unable to fetch bookings for user ${userUUID}.\n${e}`, BookingsErrorType.INTERNAL);
         }
